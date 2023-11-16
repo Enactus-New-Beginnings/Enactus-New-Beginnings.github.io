073de013de718c9a166758ea4193dd524229c49d
James Wang <james.wang77@icloud.com>
2023-11-15 22:15:32 -0500

    test

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,7 +20,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="title">New <br/>Beginnings</h1>
         <div className='box'>
-            <p style={{color: 'black'}}>Our mission is to help formerly incarcerated individuals secure employment, establish strong relationships with mentors, and gain access to local resources in order to ease their transition from prison.</p>
+            <p style={{color: 'black'}}>Our mission is to help formerly incarcerated individuals secure employment, establish strong relationships with mentors, and gain access to local resources in order to ease their transition from prison</p>
         </div>
       </header>
     <div className='features'>
